dcb9d238055bcff0d0e01dc1c989ded65bc7ca0b
jiminseong <iamjms4237@gachon.ac.kr>
2025-05-01 19:16:50 +0900

    feat:axios->fetch

--- src/views/logs-main/api/getLogsStream.ts
+++ src/views/logs-main/api/getLogsStream.ts
@@ -1,9 +1,6 @@
-import axios from "axios";
-
 export const getLogs = async (): Promise<string[]> => {
-  const res = await axios.get("https://cbh.kro.kr/logs", {
-    responseType: "text", // 응답을 string으로 받기 위해 설정
-  });
+  const res = await fetch("https://cbh.kro.kr/logs");
+  const text = await res.text();
 
-  return res.data.split("\n").filter(Boolean);
+  return text.split("\n").filter(Boolean);
 };
